aea856746df7de964854c2d8808c273d1bc527b7
iBek203 <oybekerkinovich05@gmail.com>
2026-05-20 23:52:18 +0500

    format backend.tf file

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket         = "task-manager-tfstate-infra"
-    key            = "terraform.tfstate"
-    region         = "us-east-1"
+    bucket       = "task-manager-tfstate-infra"
+    key          = "terraform.tfstate"
+    region       = "us-east-1"
     use_lockfile = true
     encrypt      = true
   }

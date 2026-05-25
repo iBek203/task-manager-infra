1684f102d68a73aa37492bf98226ca98fd885bd3
iBek203 <oybekerkinovich05@gmail.com>
2026-05-25 22:04:27 +0500

    database automate creation

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -6,6 +6,10 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.0"
+    }
   }
 }
 
@@ -17,5 +21,15 @@ provider "aws" {
       Project   = var.project
       ManagedBy = "terraform"
     }
+  }
+}
+
+provider "kubernetes" {
+  host                   = aws_eks_cluster.main.endpoint
+  cluster_ca_certificate = base64decode(aws_eks_cluster.main.certificate_authority[0].data)
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    args        = ["eks", "get-token", "--cluster-name", aws_eks_cluster.main.name]
   }
 }
---
+++ terraform/secrets.tf
@@ -0,0 +1,35 @@
+locals {
+  db_url = "postgresql://${var.db_username}:${var.db_password}@${aws_db_instance.main.address}:5432/taskmanager"
+}
+
+resource "kubernetes_namespace" "prod" {
+  metadata { name = "prod" }
+
+  depends_on = [aws_eks_node_group.prod]
+}
+
+resource "kubernetes_namespace" "dev" {
+  metadata { name = "dev" }
+
+  depends_on = [aws_eks_fargate_profile.dev]
+}
+
+resource "kubernetes_secret" "db_secret_prod" {
+  metadata {
+    name      = "db-secret"
+    namespace = kubernetes_namespace.prod.metadata[0].name
+  }
+  data = {
+    DATABASE_URL = local.db_url
+  }
+}
+
+resource "kubernetes_secret" "db_secret_dev" {
+  metadata {
+    name      = "db-secret"
+    namespace = kubernetes_namespace.dev.metadata[0].name
+  }
+  data = {
+    DATABASE_URL = local.db_url
+  }
+}

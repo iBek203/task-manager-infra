01844dd0bfa78265f1156ca4523807382bae7545
iBek203 <oybekerkinovich05@gmail.com>
2026-05-26 18:21:04 +0500

    fix: remove ecr-ci reference

--- terraform/github-oidc.tf
+++ terraform/github-oidc.tf
@@ -62,6 +62,24 @@ resource "aws_iam_role_policy" "github_actions" {
   })
 }
 
+resource "aws_eks_access_entry" "github_actions" {
+  cluster_name  = aws_eks_cluster.main.name
+  principal_arn = aws_iam_role.github_actions.arn
+  type          = "STANDARD"
+}
+
+resource "aws_eks_access_policy_association" "github_actions" {
+  cluster_name  = aws_eks_cluster.main.name
+  principal_arn = aws_iam_role.github_actions.arn
+  policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+
+  access_scope {
+    type = "cluster"
+  }
+
+  depends_on = [aws_eks_access_entry.github_actions]
+}
+
 output "github_actions_role_arn" {
   value = aws_iam_role.github_actions.arn
 }

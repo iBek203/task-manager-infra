d600b4d7e19ebc6ada65fb843339c2bcd1bbacdf
iBek203 <oybekerkinovich05@gmail.com>
2026-06-03 20:54:25 +0500

    split IAM bootstrap into separate Terraform state

--- terraform/github-oidc.tf
+++ terraform/github-oidc.tf
@@ -1,47 +1,31 @@
-resource "aws_iam_openid_connect_provider" "github" {
-  url             = "https://token.actions.githubusercontent.com"
-  client_id_list  = ["sts.amazonaws.com"]
-  thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"]
-}
-
-resource "aws_iam_role" "github_actions" {
+data "aws_iam_role" "github_actions" {
   name = "${var.project}-github-actions"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Effect = "Allow"
-      Principal = {
-        Federated = aws_iam_openid_connect_provider.github.arn
-      }
-      Action = "sts:AssumeRoleWithWebIdentity"
-      Condition = {
-        StringLike = {
-          "token.actions.githubusercontent.com:sub" = "repo:iBek203/*:*"
-        }
-        StringEquals = {
-          "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
-        }
-      }
-    }]
-  })
 }
 
-resource "aws_iam_role_policy" "github_actions" {
-  name   = "github-actions"
-  role   = aws_iam_role.github_actions.name
-  policy = file("${path.module}/github-actions-policy.json")
+removed {
+  from = aws_iam_openid_connect_provider.github
+  lifecycle { destroy = false }
+}
+
+removed {
+  from = aws_iam_role.github_actions
+  lifecycle { destroy = false }
+}
+
+removed {
+  from = aws_iam_role_policy.github_actions
+  lifecycle { destroy = false }
 }
 
 resource "aws_eks_access_entry" "github_actions" {
   cluster_name  = aws_eks_cluster.main.name
-  principal_arn = aws_iam_role.github_actions.arn
+  principal_arn = data.aws_iam_role.github_actions.arn
   type          = "STANDARD"
 }
 
 resource "aws_eks_access_policy_association" "github_actions" {
   cluster_name  = aws_eks_cluster.main.name
-  principal_arn = aws_iam_role.github_actions.arn
+  principal_arn = data.aws_iam_role.github_actions.arn
   policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
 
   access_scope {
@@ -52,5 +36,5 @@ resource "aws_eks_access_policy_association" "github_actions" {
 }
 
 output "github_actions_role_arn" {
-  value = aws_iam_role.github_actions.arn
+  value = data.aws_iam_role.github_actions.arn
 }

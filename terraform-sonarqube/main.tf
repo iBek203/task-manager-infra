4460df9b85fe41b13bcf5572411b78af6405415f
iBek203 <oybekerkinovich05@gmail.com>
2026-06-04 09:25:24 +0500

    fix az for sonarqube ec2

--- terraform-sonarqube/main.tf
+++ terraform-sonarqube/main.tf
@@ -23,7 +23,7 @@ resource "aws_internet_gateway" "sonarqube" {
 resource "aws_subnet" "sonarqube" {
   vpc_id                  = aws_vpc.sonarqube.id
   cidr_block              = "10.10.0.0/24"
-  availability_zone       = "us-east-1a"
+  availability_zone       = "${var.aws_region}b"
   map_public_ip_on_launch = true
 
   tags = { Name = "task-manager-sonarqube-subnet" }
